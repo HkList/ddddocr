2cc1a6f6f5963144ad439378aee2d2f184377042
huankong233 <huank0ng@foxmail.com>
2025-04-26 19:02:11 +0800

    [*] 支持登陆验证码解析

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import ddddocr
 import onnxruntime
 import requests
 import uvicorn
+import time
 
 onnxruntime.set_default_logger_severity(3)
 ocr = ddddocr.DdddOcr()
@@ -10,7 +11,16 @@ app = FastAPI()
 
 
 @app.get("/")
-def get_baidu_vcode():
+def get_baidu_vcode(type="sharedownload", vcode_str=None):
+    if type == "sharedownload":
+        return handleSharedownload()
+    elif type == "login":
+        return handleLogin(vcode_str)
+    else:
+        return {"code": 400, "message": "请求参数错误", "data": None}
+
+
+def handleSharedownload():
     while True:
         # 获取百度验证码信息
         try:
@@ -30,6 +40,35 @@ def get_baidu_vcode():
         # 下载验证码图片
         try:
             img_resp = requests.get(vcode_img_url, timeout=30)
+        except:
+            return {"code": 500, "message": "验证码图片下载失败", "data": None}
+
+        if img_resp.status_code != 200:
+            return {"code": 500, "message": "验证码图片下载失败", "data": None}
+
+        # 识别验证码
+        image_bytes = img_resp.content
+        result = ocr.classification(image_bytes)
+        if len(result) == 4:
+            return {
+                "code": 200,
+                "message": "请求成功",
+                "data": {"vcode_str": vcode_str, "vcode_input": result},
+            }
+
+
+def handleLogin(vcode_str):
+    while True:
+        if not vcode_str:
+            return {"code": 400, "message": "请求参数错误", "data": None}
+
+        # 下载验证码图片
+        try:
+            timestamp = int(time.time() * 1000)
+            img_resp = requests.get(
+                f"https://wappass.baidu.com/cgi-bin/genimage?{vcode_str}&v={timestamp}",
+                timeout=30,
+            )
         except:
             return {"code": 500, "message": "验证码图片下载失败", "data": None}
 
